e7474fc0c59739bcad62619f134d9a2a80991a47
Kevyn <kevyn.guima@gmail.com>
2024-01-22 20:15:46 -0300

    comments dubladores.js

--- public/dist/js/dubladores.js
+++ public/dist/js/dubladores.js
@@ -1,56 +1,58 @@
 //jeito moderno de escrever uma função, antigamente tinha que escrever assim: "function btnNovoClick() {"
 const btnNovoClick = () => {
 	
-	//# representa id e . = class  id="nome" class="nome"
-	//estou pegando o formulario onde esta id="form" <form id="form" method="post"
+	// pega o formulário onde está id="form" <form id="form" method="post" (id é # e class é .)
 	const formulario = document.querySelector('#form');//pode usar tbm document.getElementById('form')
 	
-	//resetando o formulario ou seja limpando todos os campos, mas lembrando que esse reset nao funciona em todos os tipos de campos	
+	// resetando o formulario ou seja limpando todos os campos, mas lembrando que esse reset nao funciona em todos os tipos de campos	
 	formulario.reset();
 	
-	//por exemplo campo file o reset nao funciona
+	// por exemplo campo file o reset não funciona
 	document.querySelector("input[type='file']").value = '';
 	
-	//campos ocultos(invisiveis)  tbm o reset nao funciona <input type="hidden" name="id" id="id"
+	// campos ocultos(invisiveis) tambem o reset não funciona <input type="hidden" name="id" id="id"
 	document.getElementById('id').value = '';
 	
-	//se clica no botao novo o formulario vai ser enviado para o insert
+	// se clica no botão novo, o formulário vai ser enviado para o insert
 	formulario.action = 'dubladores/insert';
 
-	// pego o titulo da modal para escrever 'Novo'	
+	// pego o título da modal para escrever 'Novo'	
 	const tituloElement = document.querySelector('#titulo');
 	if (tituloElement) {
 		tituloElement.textContent = 'Novo';
 	}
 	
-	//seto uma imagem padrao se nao foi feito nenhum upload
+	// coloca uma imagem padrão se não foi feito nenhum upload
 	document.querySelector('#retrato').src = 'dist/images/dubladores/retrato.png';
 
-	//seto o campo data_falecimento para ocultar ele
+	// coloca o campo data_falecimento no "oculto"
 	$('#collapseFalecimento').collapse('hide');
 
-	//presta atenção é importante setar tudo para evitar bugs, pensa assim vc vai clicar no botao novo, depois do editar, e depois em novo dai se nao setar os campos pode bagunçar tudo
+	// Preste atenção! É importante setar tudo para evitar bugs, pense se você vai clicar no botao novo,
+	// editar, e depois em novo. Se não setar os campos pode bagunçar tudo
 	
-	//mando abrir a modal
+	// mando abrir a modal
 	$('#modal').modal();
 }
 
 const btnEditarClick = (event) => {
-	//event ele pega muita coisa mas no caso estamos pegando quem clicou no botao
+	// event ele pega muita coisa mas no caso estamos pegando quem clicou no botão
 	btn  = event.currentTarget;
 	
-	//aqui vc ja sabe senao olha a função btnNovoClick
+	// pega o formulário onde está id="form", pois toda função precisa criar variáveis novamente 
 	const formulario = document.querySelector('#form');
 	
-	//localStorage é um jeito de salvar os dados no navegador do usuario e pegar usando javascript setItem(), getItem() , removeItem() e clear()
+	// localStorage é um jeito de salvar os dados no navegador do usuário e pegar usando javascript por setItem(),
+	// getItem(), removeItem() e clear()
 	localStorage.removeItem('data_falecimento');
-	//resetando
+	// resetando
 	formulario.reset();
 	document.querySelector("input[type='file']").value = '';
 	document.querySelector('input[name="data_falecimento"]').value = '';
 	formulario.action = 'dubladores/update';
 
-	//resumidamente quando vc ver $('algo) é jquery, jquery é muito gigante e facilita muito ele resume muito o que voce precisa fazer
+	// Resumidamente quando você ver $('algo') é jquery, jquery pode ser muito grande, porém 
+	// diminui muitas linhas de código usá-lo
 	$('#modal').off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
 		$('#collapseFalecimento').collapse('hide');
 	});	
@@ -61,12 +63,12 @@ const btnEditarClick = (event) => {
 	let tituloElement = document.querySelector('#titulo');
 	if (tituloElement) {
 		tituloElement.textContent = 'Editar';
-	} 
+	}
 	
-	//O método closest() é um método JavaScript utilizado para encontrar o ancestral mais próximo de um elemento que corresponda a um determinado seletor.
-	//ele é usado para encontrar a linha (<tr>) da tabela que contém o botão (btn) que foi clicado:
+	// O método closest() é um método JavaScript utilizado para encontrar o antecessor mais próximo de um elemento que
+	// corresponda a um determinado seletor.
+	// Ele é usado para encontrar a linha (<tr>) da tabela que contém o botão (btn) que foi clicado:
 	// por isso que ele retorna é um tr <tr>
-	
 	tr = btn.closest('tr');
 	
 	let tds = tr.children;
@@ -74,20 +76,29 @@ const btnEditarClick = (event) => {
 	console.log(tds[1].textContent.trim());
 	console.log(tds[2].textContent.trim());
 	
-	//<tr data-info='{{ {"dublador": dublador} | json_encode }}'>
-	let dataInfo        = tr.dataset.info;
+	// <tr data-info='{{ {"dublador": dublador} | json_encode }}'>
+	// todo o objeto enviado nesse data-info passa para decodificação json
+	let dataInfo         = tr.dataset.info;
+	// e assim que ele é enviado :
+	// JSON.parse(dataInfo) é o json_decode 
+	//<tr data-info="{&quot;dublador&quot;:{
+	// &quot;id&quot;:11,&quot;nome&quot;:&quot;Cassius Romero&quot;,&quot;data_nascimento&quot;:&quot;2023-01-01&quot;,
+	// &quot;data_falecimento&quot;:&quot;2020-10-05&quot;,&quot;imagem&quot;:&quot;Cassius Romero.jpg&quot;,
+	// &quot;sexo&quot;:&quot;M&quot;
+  //}}" role="row" class="odd">
+	// porém ele faz parse(dataInfo) para a variavel info, e permite usar como um array
+	let info             = JSON.parse(dataInfo);
 
-	//JSON.parse(dataInfo) é o json_decode 
-	//<tr data-info="{&quot;dublador&quot;:{&quot;id&quot;:11,&quot;nome&quot;:&quot;Cassius Romero&quot;,&quot;data_nascimento&quot;:&quot;2023-01-01&quot;,&quot;data_falecimento&quot;:&quot;2020-10-05&quot;,&quot;imagem&quot;:&quot;Cassius Romero.jpg&quot;,&quot;sexo&quot;:&quot;M&quot;}}" role="row" class="odd">										
-	let info            = JSON.parse(dataInfo);
-	
-	
-	let id              = info.dublador.id;
-	let nome            = info.dublador.nome;
-	let sexo            = info.dublador.sexo;
-	let imagem          = info.dublador.imagem;
-	let data_nascimento = info.dublador.data_nascimento;
-	let data_falecimento  = (info.dublador.data_falecimento === null || info.dublador.data_falecimento === 'null') ? '' : info.dublador.data_falecimento;
+	let id               = info.dublador.id;
+	let nome             = info.dublador.nome;
+	let sexo             = info.dublador.sexo;
+	let imagem           = info.dublador.imagem;
+	let data_nascimento  = info.dublador.data_nascimento;
+	// aqui tem uma tratativa de erro para caso esteja null ou 'null', não é colocado valor em data_falecimento.
+	// caso contrário, coloque a data que foi recebida
+	let data_falecimento = (
+		info.dublador.data_falecimento === null || info.dublador.data_falecimento === 'null'
+	) ? '' : info.dublador.data_falecimento;
 
 	$('#modal').off('shown.bs.modal').on('shown.bs.modal', function (e) {
 		if (data_falecimento !== null && data_falecimento !== 'null' && data_falecimento.length != 0) {
@@ -98,34 +109,35 @@ const btnEditarClick = (event) => {
 		}
 	});
 
-	////localStorage é um jeito de salvar os dados no navegador do usuario e pegar usando javascript setItem(), getItem() , removeItem() e clear()
-	//estou salvando os dados que pegamos do banco que estavam na tabela na linha <tr> para comparar depois no submit se teve alguma alteração no formulario
+	// aqui está salvando os dados que pegamos do banco que estavam na tabela na linha <tr> para comparar depois no
+	// submit se teve alguma alteração no formulário
 	localStorage.setItem('nome', nome);
 	localStorage.setItem('sexo', sexo);
 	localStorage.setItem('data_nascimento', data_nascimento);
 	localStorage.setItem('data_falecimento', data_falecimento);
 	
-	//preenchendo os dados do formulario
+	// preenchendo os dados do formulario
 	document.querySelector('input[name="id"]').value = id;
 	document.querySelector('input[name="nome"]').value = nome;
-	//aqui aprendeu essa dica nova?
+	// aqui é colocado exatamente o que foi recebido na variavel sexo (sendo 'M' para masculino ou 'F' para feminino) 
+	// para [value="'+sexo+'"] no input e habilita o .checked = true para que marque essa opção no sexo
 	document.querySelector('input[name="sexo"][value="' + sexo + '"]').checked = true;
 	document.querySelector('input[name="data_nascimento"]').value = data_nascimento;
 	
-	//verifica se tem imagem
+	// verifica se tem imagem
 	if(imagem) {
 		document.querySelector('#retrato').src = 'dist/images/dubladores/' + imagem;
-	
+		// se tiver data_falecimento, coloca a imagem em preto e branco
 		if(data_falecimento) {	
 			document.querySelector('#retrato').style.filter = 'grayscale(100%)';
 		}
 	}
 	
-	//abre a modal ja com o formulario preenchido para edição
+	//abre a modal ja com o formulário preenchido para edição
 	$('#modal').modal();	
 }
 
-function btnDeletarClick(event) {
+const btnDeletarClick = (event) => {
 	const btn      = event.currentTarget;
 	tr             = btn.closest('tr');
 	const dataInfo = tr.dataset.info;
@@ -143,7 +155,7 @@ function btnDeletarClick(event) {
 	  confirmButtonText: 'Sim, deletar!'
 	}).then((result) => {
 		if (result.isConfirmed) {		  
-			fetch('/animes/delete/' + id, {
+			fetch('/dubladores/delete/' + id, {
 				method: 'DELETE',
 				headers: {
 					'Content-Type': 'application/json'
@@ -193,8 +205,10 @@ function Submit(event) {
 	let imagem           = formData.get('imagem');
 
 	if(id != '') {	
-		if(nomeOrig === nome && sexoOrig === sexo && data_nascimentoOrig === data_nascimento && 
-			data_falecimentoOrig === data_falecimento && imagem.size === 0) {
+		if(
+				nomeOrig === nome && sexoOrig === sexo && data_nascimentoOrig === data_nascimento && 
+				data_falecimentoOrig === data_falecimento && imagem.size === 0
+			) {
 			Swal.fire({
 			  position: 'top-end',
 			  icon: 'info',
@@ -297,9 +311,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		}, 500);
 	});
 
-    let cells = document.querySelectorAll('.imagem-cell');
-    cells.forEach(function(cell) {
-        cell.addEventListener('click', function() {
+	let cells = document.querySelectorAll('.imagem-cell');
+	cells.forEach(function(cell) {
+		cell.addEventListener('click', function() {
 			document.querySelector('#modalImagem').style.filter = '';
 			
 			let falecimentoImg = this.getAttribute('data-falecimento');
@@ -322,8 +336,8 @@ document.addEventListener('DOMContentLoaded', () => {
 			if (modalRetrato) {
 				modalRetrato.innerHTML = this.getAttribute('data-nome');
 			}
-	
-            $('#imagemModal').modal('show');
-        });
-    });
+
+			$('#imagemModal').modal('show');
+		});
+	});
 });
